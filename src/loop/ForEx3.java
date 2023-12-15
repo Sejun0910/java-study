ba37089f319c36999d2f654c2706485cb2a6887b
Sejun0910 <choisejun99@gmail.com>
2023-12-15 11:04:06 +0900

    반복문 공부

---
+++ src/loop/ForEx3.java
@@ -0,0 +1,14 @@
+package loop;
+
+public class ForEx3 {
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int max=100, i=1; i<=max; i++) {
+            sum+= i;
+
+        }
+        System.out.println(sum);
+    }
+}
