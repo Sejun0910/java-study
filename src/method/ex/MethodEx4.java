8bddb7ea5e102420f99c4d9829034d8cc1990269
Sejun0910 <choisejun99@gmail.com>
2024-01-02 23:36:34 +0900

    메서드

--- src/method/ex/MethodEx4.java
+++ src/method/ex/MethodEx4.java
@@ -8,9 +8,9 @@ public class MethodEx4 {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("------------------");
+            System.out.println("------------------------------------");
             System.out.println("1. 입금 | 2. 출금 | 3. 잔액 환인 | 4. 종료");
-            System.out.println("------------------");
+            System.out.println("------------------------------------");
             System.out.println("선택 : ");
 
             int choice = scanner.nextInt();
